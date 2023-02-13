245faca0ddfaf5b87a321df97eade17e6a52966e
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-02-13 18:23:46 +0200

    well done

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+function fetchCountries(name) {
+    return fetch(
+        `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
+    ).then(response => {
+        if (!response.ok || response.status === 404) {
+            throw Error('Oops, there is no country with that name');
+        }
+        return response.json();
+    });
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,67 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 import debounce from 'lodash.debounce'
-
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
     searchBox: document.querySelector('#search-box'),
-    list: document.querySelector('country-list'),
-    info: document.querySelector('country-info')
+    list: document.querySelector('.country-list'),
+    info: document.querySelector('.country-info')
 }
 
 const { searchBox, list, info } = refs
 
-const handleInput = () => {
-    const searchValue = searchBox.value.trim()
-    console.log(searchValue);
-}
+const debounced = debounce(() => {
+    
+    if (searchBox.value.trim() === "") {
+        list.textContent = "";
+        info.textContent = "";
+        return
+    }
 
-searchBox.addEventListener('input', handleInput)
+    fetchCountries(searchBox.value.trim())
+        .then(result => {
+            list.textContent = "";
+            info.textContent = "";
+
+            if (result.length > 10) {
+                return Notify.info(
+                    'Too many matches found. Please enter a more specific name.'
+                );
+            } else if (10 > result.length > 1) {
+                const markup = result.map(
+                    ({ name, flags }) =>
+                        `<li>
+  <div class="wrapper">
+    <img src="${flags.svg}" alt="flag" width="50">
+    <h3>${name.common}</h3>
+  </div>
+</li>`
+                )
+                list.insertAdjacentHTML('beforeend', markup.join(''))
+            } else {
+                markup = result.map(
+                    ({ name, flags, capital, population, languages }) =>
+                        `<div class="wrap">
+  <img src=${flags.svg} alt="flag" width="70"/>
+  <h2>${name.common}</h2>
+          </div>
+          <p><strong>Capital:</strong> ${capital}</p>
+          <p><strong>Population:</strong> ${population}</p>
+          <p><strong>Languages:</strong> ${Object.values(languages)}</p>`
+                )
+                list.insertAdjacentHTML('beforeend', markup.join(''))
+            }
+        })
+        .catch(error => {
+            Notify.failure(error.message)
+            list.textContent = "";
+            info.textContent = "";
+        })
+
+}, DEBOUNCE_DELAY);
+
+searchBox.addEventListener('input', debounced)
+
